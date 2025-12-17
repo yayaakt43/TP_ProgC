cddf28802afc3bf833b00a1d51183ad54e8849be
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2025-12-17 19:52:58 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int compteur = 5;   // Doit être strictement inférieur à 10
+    int i, j;
+
+    for (i = 1; i <= compteur; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (j == 1 || j == i || i == compteur)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
