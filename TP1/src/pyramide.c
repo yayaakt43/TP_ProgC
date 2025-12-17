aea0229989c1652022d7e7d508b299144259bd72
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2025-12-17 20:07:20 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -2,25 +2,36 @@
 
 int main(void)
 {
-    int compteur = 5;   // Doit être strictement inférieur à 10
+    int n = 5;      // hauteur de la pyramide
     int i, j;
 
-    for (i = 1; i <= compteur; i++)
+    // Génération de la pyramide
+    for (i = 1; i <= n; i++)
     {
+        // Affichage des espaces pour centrer la pyramide
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+
+        // Affichage des nombres croissants
         for (j = 1; j <= i; j++)
         {
-            if (j == 1 || j == i || i == compteur)
-            {
-                printf("* ");
-            }
-            else
-            {
-                printf("# ");
-            }
+            printf("%d", j);
         }
+
+        // Affichage des nombres décroissants
+        for (j = i - 1; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+
+        // Passage à la ligne suivante
         printf("\n");
     }
 
+    // Message de fin
+    printf("Génération de la pyramide terminée.\n");
+
     return 0;
 }
-
