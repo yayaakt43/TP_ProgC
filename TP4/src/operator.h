b9f2a9e9b76bbb8ace6de71cdff4c98b6a58b7a3
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2025-12-17 21:08:04 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,13 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+int somme(int a, int b);
+int difference(int a, int b);
+int produit(int a, int b);
+int quotient(int a, int b);
+int modulo(int a, int b);
+int et(int a, int b);
+int ou(int a, int b);
+int negation(int a);
+
+#endif
