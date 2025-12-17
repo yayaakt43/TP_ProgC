443e1a0ed8a14c66855b8a232e22bdc6fe84ffda
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2025-12-17 21:08:31 +0100

    Update operator.c

---
+++ TP4/src/operator.c
@@ -0,0 +1,10 @@
+#include "operator.h"
+
+int somme(int a, int b) { return a + b; }
+int difference(int a, int b) { return a - b; }
+int produit(int a, int b) { return a * b; }
+int quotient(int a, int b) { return a / b; }
+int modulo(int a, int b) { return a % b; }
+int et(int a, int b) { return a & b; }
+int ou(int a, int b) { return a | b; }
+int negation(int a) { return ~a; }
