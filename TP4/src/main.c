f1f17294ed18249cb5b7f0f8551072e23945f214
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2025-12-17 21:09:20 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -37,3 +37,34 @@ int main(void) {
     return 0;
 }
 
+
+void exercice_4_1(void) {
+    int num1, num2;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+    printf("Entrez l'opérateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op);
+
+    int resultat = 0;
+
+    switch (op) {
+        case '+': resultat = somme(num1, num2); break;
+        case '-': resultat = difference(num1, num2); break;
+        case '*': resultat = produit(num1, num2); break;
+        case '/': resultat = quotient(num1, num2); break;
+        case '%': resultat = modulo(num1, num2); break;
+        case '&': resultat = et(num1, num2); break;
+        case '|': resultat = ou(num1, num2); break;
+        case '~': resultat = negation(num1); break;
+        default:
+            printf("Opérateur invalide\n");
+            return;
+    }
+
+    printf("Résultat : %d\n", resultat);
+}
+
