b9eb6902b327883ec8889abd46e599fa834ca8da
yanis akrout <105445785+yayaakt43@users.noreply.github.com>
2025-12-17 21:07:40 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "operator.h"
+#include "fichier.h"
+#include "liste.h"
+
+/* Prototypes des fonctions d’entrée */
+void exercice_4_1(void);
+void exercice_4_2(void);
+void exercice_4_7(void);
+
+int main(void) {
+    int choix;
+
+    printf("Choisissez l'exercice à exécuter :\n");
+    printf("1 - Exercice 4.1 : Calcul avec opérateurs\n");
+    printf("2 - Exercice 4.2 : Gestion de fichiers\n");
+    printf("3 - Exercice 4.7 : Liste de couleurs\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+
+    switch (choix) {
+        case 1:
+            exercice_4_1();
+            break;
+        case 2:
+            exercice_4_2();
+            break;
+        case 3:
+            exercice_4_7();
+            break;
+        default:
+            printf("Choix invalide.\n");
+    }
+
+    return 0;
+}
 
